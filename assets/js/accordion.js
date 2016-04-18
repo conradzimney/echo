b47063d5d27174c1ce864f226010e519757556a4
Dillon Byron <dillonbyron@Dillons-MBP.hsd1.wa.comcast.net>
2016-04-17 23:22:26 -0700

    content update

---
+++ assets/js/accordion.js
@@ -0,0 +1,18 @@
+"use strict";
+
+$(document).ready(function() {
+    $("#slider").zAccordion({
+        tabWidth: 170,
+        speed: 650,
+        slideClass: 'slider',
+        animationStart: function () {
+            $('#slider').find('li.slider-open div').css('display', 'none');
+            $('#slider').find('li.slider-previous div').css('display', 'none');
+    },
+    animationComplete: function () {
+        $('#slider').find('li div').fadeIn(600);
+    },
+    width: window.innerWidth - 310,
+    height: 600
+    });
+});
